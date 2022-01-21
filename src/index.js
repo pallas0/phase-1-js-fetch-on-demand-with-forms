7620362cd24638392000fc99aff2f89f18e9808e
amelia.risner0@gmail.com <arisner@berkeley.edu>
2022-01-20 21:24:22 -0800

    DONE

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
-const init = () => {
-  
-}
+fetch("http://localhost:3000/movies")
+.then((res) => res.json())
+.then((data) =>  {
+    const arrayMovies = data;
+    const movieForm = document.querySelector('form')
 
-document.addEventListener('DOMContentLoaded', init);
+    movieForm.addEventListener("submit", function(e) {
+        e.preventDefault()
+        const movieSearch = document.getElementById("searchByID").value
+        let searchResult = ""
+
+        arrayMovies.forEach((movie) => {
+            if (movie.id == movieSearch) {
+                searchResult = movie
+            }
+        })
+        //console.log(searchResult.summary)
+
+        const movieTitle = document.getElementById("title");
+        const movieSummary = document.getElementById("summary")
+        movieTitle.innerText = searchResult.title
+        movieSummary.innerText = searchResult.summary
+
+
+        
+    })
+})
+
+
+
